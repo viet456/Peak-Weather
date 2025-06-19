22bc48242405a967352e10e3e3843878bde5cf5c
lennking <vikkerz456@gmail.com>
2025-06-18 18:57:22 -0700

    feat(search): render location autocomplete dropdown

--- src/main.js
+++ src/main.js
@@ -2,4 +2,33 @@ import './style.css'
 import { getWeather } from './api/weather'
 import { getLocations } from './api/locations';
 
-getWeather(37.77, 122.41);
+// check location matches
+const searchResults = document.getElementById('search-results');
+async function showLocationDropdown(input) {
+  if (!input) {
+    return null;
+  }
+  searchResults.innerHTML = '';
+  let locations = await getLocations(input);
+  locations.forEach(location => {
+    const locationEl = document.createElement('div');
+    locationEl.classList.add('locationEl');
+
+    // only include admin1 for countries that use states/provinces
+    const stateCountries = ['United States', 'Canada', 'Australia', 'Brazil', 'India', 'Mexico', 'United Kingdom', 'Argentina'];
+    const locationText = stateCountries.includes(location.country)
+                        ? `${location.name}, ${location.admin1}, ${location.country}`
+                        : `${location.name}, ${location.country}`;
+    locationEl.textContent = locationText;
+    searchResults.append(locationEl);
+  });;
+} 
+
+let timeout;
+const search = document.getElementById('search');
+search.addEventListener('input', (e) => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    showLocationDropdown(e.target.value);
+  }, 300);
+});
