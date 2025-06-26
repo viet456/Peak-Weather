ceb1f329c6d2b006867eb5d0ba074be1d29d0666
lennking <vikkerz456@gmail.com>
2025-06-26 03:28:10 -0700

    feat(daily): Style Daily forecast component

--- src/ui/displayDaily.js
+++ src/ui/displayDaily.js
@@ -1,5 +1,9 @@
 import { formatLocationForDisplay } from "../utils/formatLocation";
 import { formatTime, formatTemp, formatSpeed, formatPrecipitation, formatDayOfWeek } from "../utils/formatter";
+import { getWeatherDescription } from "../utils/weatherDataProcessor";
+import { getWeatherIcon } from "../utils/weatherIcons";
+import { getWeatherIconName } from "../utils/getWeatherIconName";
+import './daily.css';
 
 function renderDailyItem(dayData, systemUnit) {
     const itemTemplate = document.getElementById('daily-item-template');
@@ -8,21 +12,35 @@ function renderDailyItem(dayData, systemUnit) {
     const elements = {
         day: itemClone.querySelector('.js-daily-day'),
         icon: itemClone.querySelector('.js-daily-icon'),
+        text: itemClone.querySelector('.js-daily-forecast'),
         high: itemClone.querySelector('.js-daily-high'),
         low: itemClone.querySelector('.js-daily-low'),
-        precip: itemClone.querySelector('.js-daily-precip'),
+        precipIcon: itemClone.querySelector('.js-daily-precip-icon'),
+        precipText: itemClone.querySelector('.js-daily-precip-text'),
     };
 
     const weekday = formatDayOfWeek(dayData.date);
     const highTemp = formatTemp(dayData.tempMax, systemUnit);
     const lowTemp = formatTemp(dayData.tempMin, systemUnit);
+    const description = getWeatherDescription(dayData.weatherCode);
+    const iconName = getWeatherIconName(dayData);
+    const iconSvg = getWeatherIcon(iconName);
     const precipProbability = dayData.precipProbabilityMax;
+    let precipSvg = '';
+    if (precipProbability >= 0) {
+        precipSvg = getWeatherIcon('droplet-empty');
+        if (precipProbability >= 30) {
+            precipSvg = getWeatherIcon('droplet-filled');
+        }
+    };
 
     elements.day.textContent = weekday;
-    elements.icon.textContent = '';
+    elements.icon.innerHTML = `${iconSvg}`;
+    elements.text.textContent = description;
     elements.high.textContent = highTemp;
     elements.low.textContent = lowTemp;
-    elements.precip.textContent = `${precipProbability}%`;
+    elements.precipIcon.innerHTML = `${precipSvg}`;
+    elements.precipText.textContent = `${precipProbability}%`;
 
     return itemClone;
 }
@@ -37,7 +55,7 @@ export function displayDaily(dailyData, location, systemUnit) {
         const dailyItemElement = renderDailyItem(day, systemUnit);
         itemsContainer.append(dailyItemElement);
     });
-    
+
     const pageContainer = document.getElementById('daily-forecast-container');
     pageContainer.innerHTML = '';
     pageContainer.append(containerClone);
