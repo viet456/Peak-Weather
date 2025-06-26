504680d41432fd78a328778239e7061f0e0f7fe0
lennking <vikkerz456@gmail.com>
2025-06-26 02:34:26 -0700

    feat(hourly): Style hourly component

--- src/ui/displayHourly.js
+++ src/ui/displayHourly.js
@@ -13,7 +13,8 @@ function renderHourlyItem(hourData, systemUnit) {
         temp: itemClone.querySelector('.js-hourly-temp'),
         icon: itemClone.querySelector('.js-hourly-icon'),
         text: itemClone.querySelector('.js-hourly-description'),
-        precip: itemClone.querySelector('.js-hourly-precip'),
+        precipIcon: itemClone.querySelector('.js-hourly-precip-icon'),
+        precipText: itemClone.querySelector('.js-hourly-precip-text'),
     }
 
     const time = formatTime(hourData.date, { includeMinutes: false });
@@ -22,12 +23,20 @@ function renderHourlyItem(hourData, systemUnit) {
     const iconName = getWeatherIconName(hourData);
     const iconSvg = getWeatherIcon(iconName);
     const precipProbability = hourData.precipProbability;
+    let precipSvg = '';
+    if (precipProbability >= 0) {
+        precipSvg = getWeatherIcon('droplet-empty');
+        if (precipProbability >= 30) {
+            precipSvg = getWeatherIcon('droplet-filled');
+        }
+    };
 
     elements.time.textContent = time;
     elements.icon.innerHTML = `${iconSvg}`;
     elements.temp.textContent = temp;
     //elements.text.textContent = weatherCode;
-    elements.precip.textContent = `${precipProbability}%`;
+    elements.precipIcon.innerHTML = `${precipSvg}`;
+    elements.precipText.textContent = `${precipProbability}%`;
 
     return itemClone;
 }
